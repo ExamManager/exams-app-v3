cc9616c965f787d1fe55d99a754df9b7a5c57be2
DaniDuese <info@bitstore.dev>
2024-03-29 22:40:21 +0100

    Update admin dashboard page layout and content

--- src/app/(app)/admin/dashboard/page.tsx
+++ src/app/(app)/admin/dashboard/page.tsx
@@ -1,13 +1,23 @@
 import { getUser } from "@/server/auth";
+import { adminDashboardPageConfig } from "@/app/(app)/admin/dashboard/_constants/page-config";
+import { AppPageShell } from "@/app/(app)/_components/page-shell";
 
 export default async function AdminDashPage() {
     const user = await getUser();
 
     return (
         <div>
-            <h1>Admin Dashboard</h1>
-            <p>Welcome {user?.name}</p>
-            <p>{user?.isNewUser ? "Yes" : "No"}</p>
+            <AppPageShell
+            title={adminDashboardPageConfig.title}
+            description={adminDashboardPageConfig.description}
+        >
+            <div className="w-full">
+                <div className="flex flex-col items-center justify-center h-96">
+                    <h1 className="text-4xl font-semibold">Welcome back, {user?.name}!</h1>
+                    <p className="text-muted-foreground">Here's an overview of your app's performance and statistics.</p>
+                </div>
+            </div>
+        </AppPageShell>
         </div>
     );
 }
